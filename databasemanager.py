abc245d46386640b63c296b52de3e6062502e6c0
thebuzzybee <schoenfeld.robert@web.de>
2026-04-08 13:37:24 +0200

    db und gui

---
+++ databasemanager.py
@@ -0,0 +1,133 @@
+﻿import sqlite3
+
+class DatabaseManager:
+    def __init__(self, db_path = "finanzen.db"):
+        self.db_path = db_path
+        
+    def _get_connection(self):
+        return sqlite3.connect(self.db_path)
+
+    def initialize_db(self):
+        conn = self._get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS categories (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                parent_id INTEGER,
+                FOREIGN KEY (parent_id) REFERENCES categories(id)
+                )
+            """)
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS transactions (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                date TEXT NOT NULL,
+                amount REAL NOT NULL,
+                type TEXT NOT NULL,
+                category_id INTEGER,
+                description TEXT,
+                FOREIGN KEY (category_id) REFERENCES categories(id)
+                )
+        """)
+
+
+
+        conn.commit()
+        conn.close()
+
+    def add_category(self, name, parent_id = None):
+        conn = self._get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO categories (name, parent_id)
+            VALUES (?, ?)
+            """, (name, parent_id))
+        conn.commit()
+        conn.close()
+
+    def get_categories(self, parent_id = None):
+        conn = self._get_connection()
+        cursor = conn.cursor()
+        if parent_id is None:
+            cursor.execute("SELECT * FROM categories WHERE parent_id is NULL",)
+        else:
+            cursor.execute("SELECT * FROM categories WHERE parent_id = ?",(parent_id,))
+        results = cursor.fetchall()
+        conn.close()
+        return results
+
+    def update_category(self, category_id, new_name):
+        conn = self._get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            UPDATE categories SET name = ? WHERE id = ?
+            """, (new_name, category_id))
+        conn.commit()
+        conn.close()
+
+    def delete_category(self, category_id):
+        conn = self._get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            DELETE FROM categories WHERE id = ?
+            """, (category_id,))
+        conn.commit()
+        conn.close()
+
+    def add_transaction(self, date, amount, type, category_id, description = None):
+        conn = self._get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO transactions (date, amount, type, category_id, description)
+            VALUES (?, ?, ?, ?, ?)
+        """, (date, amount, type, category_id, description))
+        conn.commit()
+        conn.close()
+
+    def get_transactions(self, type = None, category_id = None, start_date = None, end_date = None, search_text = None, order_by = None, order_dir = "ASC"):
+        conn = self._get_connection()
+        cursor = conn.cursor()
+
+        query = "SELECT * FROM transactions WHERE 1=1"
+        params = []
+        if type is not None:
+            query += " AND type = ?"
+            params.append(type)
+
+        if category_id is not None:
+            query += " AND category_id = ?"
+            params.append(category_id)
+
+        if start_date is not None:
+            query += " AND date >= ?"
+            params.append(start_date)
+
+        if end_date is not None:
+            query += " AND date <= ?"
+            params.append(end_date)
+
+        if search_text is not None:
+            query += " AND description LIKE ?"
+            params.append(f"%{search_text}%")
+
+        if order_by is not None:
+            allowed_columns = ["date", "amount", "type", "category_id", "description"]
+            if order_by in allowed_columns:
+                if order_dir.upper() in ["ASC", "DESC"]:
+                    query += f" ORDER BY {order_by} {order_dir.upper()}"
+                else:
+                    query += f" ORDER BY {order_by} ASC"
+
+
+
+
+        cursor.execute(query, params)
+        results = cursor.fetchall()
+        conn.close()
+        return results
+
+if __name__ == "__main__":
+    db = DatabaseManager("test.db")
+    db.initialize_db()
--- gui.py
+++ gui.py
@@ -1,16 +1,70 @@
 ﻿import customtkinter as ctk
+from customtkinter import CTkButton
+
+from databasemanager import DatabaseManager
+from tkinter import ttk
 
 class BudgetPlannerApp(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Budget Planer")
         self.geometry("1000x600")
+        
+        self.db = DatabaseManager()
+        self.db.initialize_db()
 
         self.create_widgets()
 
     def create_widgets(self):
-        label = ctk.CTkLabel(self, text="Budget Planer – GUI wird aufgebaut")
-        label.pack(pady=20)
+        self.tabview = ctk.CTkTabview(self)
+        self.tabview.pack(fill="both", expand=True)
+        
+        self.tab_transactions = self.tabview.add("Transactions")
+        self.tab_categories = self.tabview.add("Categories")
+
+        frame_tree = ctk.CTkFrame(self.tab_categories)
+        frame_tree.pack(fill="both", expand=True, padx=10, pady=10)
+        
+        self.tree = ttk.Treeview(frame_tree, columns=("ID", "Name", "Maincategory"), show="tree headings")
+        self.tree.heading("#0", text="")
+        self.tree.heading("ID", text="ID")
+        self.tree.heading("Name", text="Name")
+        self.tree.heading("Maincategory", text="Maincategory")
+        self.tree.column("#0", width=0, stretch=False)
+        self.tree.pack(fill="both", expand=True)
+        
+        frame_buttons = ctk.CTkFrame(self.tab_categories)
+        frame_buttons.pack(fill="x", padx = 10, pady = 10)
+        
+        button_add = CTkButton(frame_buttons, text = "Add")
+        button_add.pack(side="left", padx = 5)
+        
+        button_edit = CTkButton(frame_buttons, text = "Edit")
+        button_edit.pack(side="left", padx = 5)
+        
+        button_del = CTkButton(frame_buttons, text = "Delete")
+        button_del.pack(side="left", padx = 5)
+        
+        self.load_categories()
+        
+    def load_categories(self):
+        self.tree.delete(*self.tree.get_children())
+        self.category_map = {}
+        rows = self.db.get_categories()
+        for row in rows:
+            category_id, name, parent_id = row
+            if parent_id is None:
+                tree_id = self.tree.insert("", "end", values=(category_id, name, parent_id))
+                self.category_map[category_id] = tree_id
+                
+        for row in rows:
+            category_id, name, parent_id = row
+            if parent_id is not None:
+                parent_tree_id = self.category_map[parent_id]
+                tree_id = self.tree.insert(parent_tree_id, "end", values=(category_id, name, parent_id))
+                self.category_map[category_id] = tree_id            
+                
+        
 
     def run(self):
         self.mainloop()
